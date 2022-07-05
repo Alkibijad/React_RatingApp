4e18b5e5bc183606f1a006396d3fa33dd3e22abf
Alkibijad <aleksandar04081@gmail.com>
2022-07-05 13:12:26 -0700

    used .reduce on avarege rate component
    I have changed from using JS function to using .reduce method to calculate average rating

--- rating_app/src/components/AverageRate.jsx
+++ rating_app/src/components/AverageRate.jsx
@@ -1,21 +1,30 @@
 import React from "react";
 
 function AverageRate({ feedback }) {
-  let feedbackRatings = [];
+  // this is JS way of doing it
+  // let feedbackRatings = [];
 
-  feedback.forEach((item) => {
-    feedbackRatings.push(item.rating);
-  });
+  // feedback.forEach((item) => {
+  //   feedbackRatings.push(item.rating);
+  // });
 
-  let total = null;
-  for (let i = 0; i < feedbackRatings.length; i++) {
-    total += feedbackRatings[i];
-  }
-  let averageNum = total / feedbackRatings.length;
+  // let total = null;
+  // for (let i = 0; i < feedbackRatings.length; i++) {
+  //   total += feedbackRatings[i];
+  // }
+  // let averageNum = total / feedbackRatings.length;
+
+  // using addvanced array method .reduce 
+
+  let average = feedback.reduce((acc, cur) => {
+    return acc + cur.rating
+  }, 0) / feedback.length
+  
+
 
   return (
     <div className="average_num">
-      <p>The average num of your ratings is {averageNum}</p>
+      <p>The average num of your ratings is {average}</p>
     </div>
   );
 }
